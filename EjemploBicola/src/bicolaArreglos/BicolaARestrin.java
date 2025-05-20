88e2029f69574536ca47c1405bfe1343cca0f8a3
Sebastian Chavez <carlos.chavez03@epn.edu.ec>
2025-05-20 17:06:32 -0500

    Ejercicio bicola restringida de entrada

--- EjemploBicola/src/bicolaArreglos/BicolaARestrin.java
+++ EjemploBicola/src/bicolaArreglos/BicolaARestrin.java
@@ -11,19 +11,76 @@ public class BicolaARestrin {
 		this.tamanio = capacidad;
 	}
 	
+	//Comprobar si la cola está llena
 	public boolean estaVacia() {
 		return frente == -1;
 	}
 	
+	//Comprobar si la cola está vacía
 	public boolean estaLlena() {
 		return (fin + 1) % tamanio == frente;
 	}
 	
+	//Insertar nodo al final
 	public void insertarFinal(int dato) throws Exception{
-		if(estaLlena()) {
+		if(estaLlena()) { //Si está llena, no se pueden ingresar más datos en la bicola
 			throw new Exception("La bicola está llena.");
 		}
 		
+		if(estaVacia()) { 
+			frente = fin = 0; //si está vacía, fin y frente son la primera casilla del arreglo
+		}else {
+			fin = (fin + 1) % tamanio; //si está con nodos adentro, se usa esta fórmula para mostrar el índice en donde se tiene que llenar
+		}
+		arreglo[fin] = dato; //insertamos el dato en el índice sacado anteriormente
+		System.out.println("Insertado al final: " + dato);
+	}
+	
+	//Eliminar nodo del frente
+	public int eliminarFrente() throws Exception{
+		if(estaVacia()) { //Si la bicola está vacía no se puede eliminar nada
+			throw new Exception("La bicola está vacía"); //
+		}
 		
+		int valor = arreglo[frente]; //guardamos el valor de frente
+		if(frente == fin) { //Si solo hay un elemento en la cola
+			frente = fin = -1; //Mandamos frente y fin igual a -1 para indicar que no hay elementos en la cola (eliminación lógica)
+		}else {
+			frente = (frente + 1) % tamanio; //usamos esta fórmula para recorrer un nodo a la derecha y eliminar lógicamente a ese nodo.
+		}
+		System.out.println("Eliminado del frente: " + valor);
+		return valor; //retornamos el valor que se ha eliminado
+	}
+	
+	//Eliminar nodo del final
+	public int eliminarFinal() throws Exception{
+		if(estaVacia()) {
+			throw new Exception("La bicola está vacía."); //Si la cola está vacía no se pueden borrar elementos
+		}
+		
+		int valor = arreglo[fin]; //guardamos el valor del fin
+		if(frente == fin) { //si solo hay un nodo
+			frente = fin = -1; //ponemos los valores frente y fin igual a -1 para indicar que no hay elementos en la cola (eliminación lógica)
+		}else {
+			fin = (fin -1 + tamanio) % tamanio; //usamos esta fórmula para recorrer espacios a la izquierda y eliminar lógicamente a ese nodo
+		}
+		System.out.println("Eliminiado del final: " + valor);
+		return valor; //retornamos el valor que se ha eliminado
+	}
+	
+	//Imprimir cola
+	public void mostrar() {
+		if(estaVacia()) {
+			System.out.println("La bicola está vacía."); //si la bicola está vacía no se puede imprimir nada
+			return;
+		}
+		
+		System.out.println("Contenido de la bicola: ");
+		int i = frente; //usamos un auxiliar i igualado a frente para que vaya recorriendo toda la cola
+		while(true) { //ciclo infinito hasta que algo (break) lo detenga
+			System.out.print("[" + arreglo[i] + "] ");
+			if(i == fin) break; //si i llega a la posición fin, se termina el ciclo
+			i = (i + 1) % tamanio; //si no, se sigue usando la fórmula para recorrer a la derecha
+		}
 	}
 }
--- EjemploBicola/src/bicolaArreglos/PrincipalBicolaARestrin.java
+++ EjemploBicola/src/bicolaArreglos/PrincipalBicolaARestrin.java
@@ -1,10 +1,66 @@
 package bicolaArreglos;
 
+import java.util.Scanner;
+
 public class PrincipalBicolaARestrin {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		Scanner sc = new Scanner(System.in);
+		BicolaARestrin bicolaRestrin = new BicolaARestrin(3);
+		int opcion, valor;
+		do {
+			System.out.println("\n--- MENÚ BICOLA (RE) ---");
+			System.out.println("1. Insertar al final");
+			System.out.println("2. Eliminar al frente");
+			System.out.println("3. Eliminar al final");
+			System.out.println("4. Mostrar bicola");
+			System.out.println("5. Verificar si está vacía");
+			System.out.println("6. Verificar si está llena");
+			System.out.println("7. Salir");
+			System.out.println("Eliga la opción:");
+			opcion = sc.nextInt();
+			
+			switch(opcion) {
+			case 1:
+				System.out.println("Valor a insertar: ");
+				valor = sc.nextInt();
+				try {
+					bicolaRestrin.insertarFinal(valor);
+				} catch (Exception e) {
+					System.out.println(e.getMessage());
+				}
+				break;
+			case 2:
+				try {
+					bicolaRestrin.eliminarFrente();
+				} catch (Exception e) {
+					System.out.println(e.getMessage());
+				}
+				break;
+			case 3:
+				try {
+					bicolaRestrin.eliminarFinal();
+				} catch (Exception e) {
+					System.out.println(e.getMessage());
+				}
+				break;
+			case 4:
+				bicolaRestrin.mostrar();
+				break;
+			case 5:
+				System.out.println(bicolaRestrin.estaVacia()? "La bicola está vacía" : "La bicola no está vacía");
+				break;
+			case 6:
+				System.out.println(bicolaRestrin.estaLlena()? "La bicola está llena" : "La bicola no está llena");
+				break;
+			case 7:
+				System.out.println("Saliendo...");
+				break;
+				default:
+					System.out.println("Opción inválida.");
+			}
+		}while(opcion != 7);
+		sc.close();
 	}
-
 }
